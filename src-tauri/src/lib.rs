9df2b13b29eb99f623ce846fd8e83bfaf4b96fe8
Manuel Vasquez Dickson <vadmanuel@gmail.com>
2025-09-16 18:21:17 -0400

    Init

---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,16 @@
+#[cfg(any(target_os = "ios", target_os = "android"))]
+compile_error!("Mobile builds are disabled for this app.");
+
+// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+pub fn run() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_opener::init())
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
